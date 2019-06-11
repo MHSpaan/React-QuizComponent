bbaf1b1603a8a044657ea827c09a7f28b133b0f3
MHSpaan <42636211+MHSpaan@users.noreply.github.com>
2019-06-11 11:37:58 +0300

    Add files via upload

--- src/Quiz.js
+++ src/Quiz.js
@@ -25,7 +25,7 @@ class Quiz extends Component {
             <div>
                 {
                     isQuizEnd ?
-                        <QuizEnd /> :
+                        <QuizEnd resetClickHandler={this.handleResetClick.bind(this)}/> :
                         <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
                         showNextQuestionHandler={this.showNextQuestion.bind(this)} />
                 }
@@ -33,6 +33,10 @@ class Quiz extends Component {
         )
 
 
+    }
+
+    handleResetClick() {
+        this.setState({quiz_position: 1});
     }
 
 
